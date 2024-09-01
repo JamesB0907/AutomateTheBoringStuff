f1d09fb374e640ac591b64abed13b36407a59f80
James Brooks <virverum22@gmail.com>
2024-09-01 18:14:28 -0400

    Partway through Chapter 3

---
+++ Chapter3/chapter3_notes.py
@@ -0,0 +1,35 @@
+# Functions:
+
+# first example found in hello_func.py
+
+# def Statements with Parameters:
+
+# example in hello_func2.py
+
+# Define, Call, Pass, Argument, Parameter:
+
+    # Define: to create a function
+    # Call: to execute a function
+    # Pass: to give a function an argument
+    # Argument: the value passed to the function
+    # Parameter: the variable that stores the argument
+
+# Return Values and Return Statements:
+
+# when creating a function using the def statement, you can specify what the return value should be using the return statement using:
+    # the return keyword
+    # the value or expression that the function should return
+
+# example in magic_8_ball.py
+
+# The None Value:
+
+# None is a special value that represents the absence of a value. It is the only value of the NoneType data type.
+
+spam = print('Hello!')
+# -> Hello!
+None == spam
+# -> True
+
+# similar to a static void method in C#, it doesn't return anything, which, if no return is specified, the function will return None invisibly.
+
