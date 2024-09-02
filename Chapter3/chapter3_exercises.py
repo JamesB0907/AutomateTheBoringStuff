341bcd3007bd9e3191ac41f08bb70c851c6fb661
James Brooks <virverum22@gmail.com>
2024-09-02 00:46:53 -0400

    Chapter 3 finished

---
+++ Chapter3/chapter3_exercises.py
@@ -0,0 +1,55 @@
+# 1. Why are functions advantageous to have in your programs?
+
+# Functions are advantageous to have in your programs because they allow you to reuse code, make your code more readable, and make your code more modular.
+
+# 2. When does the code in a function execute: when the function is defined or when the function is called?
+
+# The code in a function executes when the function is called.
+
+# 3. What statement creates a function?
+
+# The def statement creates a function.
+
+# 4. What is the difference between a function and a function call?
+
+# A function is a block of code that performs a specific task and a function call is the code that runs the function.
+
+# 5. How many global scopes are there in a Python program? How many local scopes?
+
+# There is one global scope in a Python program and there can be multiple local scopes.
+
+# 6. What happens to variables in a local scope when the function call returns?
+
+# When the function call returns, the local scope is destroyed and the variables are forgotten.
+
+# 7. What is a return value? Can a return value be part of an expression?
+
+# A return value is the value that a function call evaluates to. A return value can be part of an expression.
+
+# 8. If a function does not have a return statement, what is the return value of a call to that function?
+
+# If a function does not have a return statement, the return value of a call to that function is None.
+
+# 9. How can you force a variable in a function to refer to the global variable?
+
+# You can force a variable in a function to refer to the global variable by using the global statement.
+
+# 10. What is the data type of None?
+
+# The data type of None is NoneType.
+
+# 11. What does the import areallyourpetsnamederic statement do?
+
+# The import areallyourpetsnamederic statement imports a module named areallyourpetsnamederic.
+
+# 12. If you had a function named bacon() in a module named spam, how would you call it after importing spam?
+
+# spam.bacon()
+
+# 13. How can you prevent a program from crashing when it gets an error?
+
+# You can prevent a program from crashing when it gets an error by using try and except statements.
+
+# 14. What goes in the try clause? What goes in the except clause?
+
+# The code that you want to run goes in the try clause and the code that you want to run if an error occurs goes in the except clause.
--- Chapter3/chapter3_notes.py
+++ Chapter3/chapter3_notes.py
@@ -31,5 +31,107 @@ spam = print('Hello!')
 None == spam
 # -> True
 
-# similar to a static void method in C#, it doesn't return anything, which, if no return is specified, the function will return None invisibly.
+# similar to a static void method in C#, it doesn't return anything, which, if no return is specified, the function will return None (invisibly).
+
+# Keyword Arguments and print() Function:
+
+# the print() function has two optional keyword arguments:
+    # sep - the separator between arguments (default is a space)
+    # end - the character that comes at the end of the string (default is a newline)
+
+# sep:
+print('Hello', 'World', sep='*')
+# -> Hello*World
+
+print('cats', 'dogs', 'mice', sep=',')
+# -> cats,dogs,mice
+
+# end:
+print('Hello', end='')
+print('World')
+# -> HelloWorld
+
+# The Call Stack:
+
+# the call stack is a way to keep track of where the program is in a function. When a function is called, the program jumps to that function and executes it, then returns to where it left off.
+
+# see abc_call_stack.py for an illustration of the call stack
+
+# Local and Global Scope:
+
+# local scope - variables that are created inside a function are local to that function and cannot be accessed outside of it
+
+# global scope - variables that are created outside of a function are global and can be accessed anywhere in the program
+
+def scope_spam():
+    eggs = 31337
+scope_spam()
+# print(eggs)
+# -> NameError: name 'eggs' is not defined
+
+def local_spam():
+    eggs = 99
+    bacon()
+    print(eggs)
+def bacon():
+    hame = 101
+    eggs = 0
+local_spam()
+# -> 99
+
+def global_spam():
+    print(eggs2)
+eggs2 = 42
+global_spam()
+print(eggs2)
+# -> 42
+
+def another_spam():
+    eggs3 = 'spam local'
+    print(eggs3) # prints 'spam local'
+def bacon2():
+    eggs3 = 'bacon local'
+    print(eggs3) # prints 'bacon local'
+    another_spam()
+    print(eggs3) # prints 'bacon local'
+eggs3 = 'global'
+bacon2()
+print(eggs3) # prints 'global'
+
+# The Global Statement:
+
+# using the global statement allows you to modify a global variable from within a function
+
+def spam_with_global_eggs():
+    global eggs
+    eggs = 'spam'
+eggs = 'global'
+spam_with_global_eggs()
+print(eggs) # -> spam
+
+# refer to same_name_local_global.py for an example of local and global variables with the same name
+
+# Exception Handling:
+
+# def exception_spam(divide_by):
+#     return 42 / divide_by
+# print(exception_spam(2))
+# print(exception_spam(12))
+# print(exception_spam(0))
+# print(exception_spam(1))
+
+# cannot divide by 0, so the program will throw an error
+
+# try and except Statements:
+
+def exception_spam2(divide_by):
+    try:
+        return 42 / divide_by
+    except ZeroDivisionError:
+        print('Error: Invalid argument.')
+print(exception_spam2(2))
+print(exception_spam2(12))
+print(exception_spam2(0))
+print(exception_spam2(1))
+
 
