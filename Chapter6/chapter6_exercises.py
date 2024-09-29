ea467943e7fc05fbb9aaa0e0b717e9c4c98c0fae
James Brooks <virverum22@gmail.com>
2024-09-29 16:10:13 -0400

    Chapter 6 Questions done

---
+++ Chapter6/chapter6_exercises.py
@@ -0,0 +1,51 @@
+# Chapter 6 Practice Questions
+
+# 1. What are escape characters?
+
+# Escape character are exempted from a string's interpretation. They are used to represent characters that are difficult or impossible to type directly. For example, the newline character is '\n'.
+
+# 2. What do the \n and \t escape characters represent?
+
+# n\ is newline and \t is tab.
+
+# 3. How can you put a \ backslash character in a string?
+
+# By using a double backslash \\.
+
+# 4. The string value "Howl's Moving Castle" is a valid string. Why isn't it a problem that the single quote character in the word Howl's isn't escaped?
+
+# Because the string is enclosed in double quotes.
+
+# 5. If you don't want to put \n in your string, how can you write a string with newlines in it?
+
+# Using ''' on both sides of the string or using the print() function again.
+
+# 6. What do the following expressions evaluate to?
+
+# 'Hello, world!'[1] = 'e'
+# 'Hello, world!'[0:5] = 'Hello'
+# 'Hello, world!'[:5] = 'Hello'
+# 'Hello, world!'[3:] = 'lo, world!'
+
+# 7. What do the following expressions evaluate to?
+
+# 'Hello'.upper() = 'HELLO'
+# 'Hello'.upper().isupper() = True
+# 'Hello'.upper().lower() = 'hello'
+
+# 8. What do the following expressions evaluate to?
+
+# 'Remember, remember, the fifth of November.'.split() = ['Remember,', 'remember,', 'the', 'fifth', 'of', 'November.']
+# '-'.join('There can be only one.'.split()) = 'There-can-be-only-one.'
+
+# 9. What string methods can you use to right-justify, left-justify, and center a string?
+
+# rjust(), ljust(), and center().
+
+# 10. How can you trim whitespace characters from the beginning or end of a string?
+
+# Using the strip() method.
+
+# See table_printer.py for the answers to the next questions.
+# See zombie_dice.py for the answers to the next questions.
+
